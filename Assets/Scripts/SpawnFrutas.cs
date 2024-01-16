2f456d03913783ba6a0be127b7d4c1f3ada1b08f
jajo560 <joseantonio.jimenez@enti.cat>
2024-01-16 11:03:42 +0100

    Fruit Drop

--- Assets/Scripts/SpawnFrutas.cs
+++ Assets/Scripts/SpawnFrutas.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class SpawnFrutas : MonoBehaviour
 {
-    
+    public GameObject raindropPrefab;
+    public float raindropSpeed;
+
     void Start()
     {
-        
+        // Inicia la lluvia en intervalos regulares
+        Invoke("SpawnRaindrop", 1);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // Puedes agregar lógica de juego aquí si es necesario
+    }
+
+   public void SpawnRaindrop()
+    {
+        // Crea una nueva gota de lluvia en una posición aleatoria en la parte superior de la pantalla
+        Vector3 spawnPosition = new Vector3(Random.Range(-8.5f, 8.5f), 7f, 0f);
+        GameObject raindrop = Instantiate(raindropPrefab, spawnPosition, Quaternion.identity);
+
+        // Configura la velocidad de la gota de lluvia
+        Rigidbody2D rb = raindrop.GetComponent<Rigidbody2D>();
+        rb.velocity = new Vector2(0, -raindropSpeed);
     }
 }
