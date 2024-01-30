5eb5c06c337cd7f938b548b5ba58ade3c0a8a0ba
MarcReyes99 <marc.reyes@enti.cat>
2024-01-30 12:07:13 +0100

    F

--- Assets/Scripts/DestroyFruit.cs
+++ Assets/Scripts/DestroyFruit.cs
@@ -5,13 +5,28 @@ using UnityEngine;
 
 public class Fruta : MonoBehaviour
 {
-    public UI punt;
+    public UI puntuacion;
     public List<ConfiguracionFruta> configuracionFrutas;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Fruta"))
         {
+            int configuraciones = GetConfiguracionFruta(collision.gameObject);
             Destroy(collision.gameObject);
+
+            if (configuraciones >= 0 && configuraciones < configuracionFrutas.Count)
+            {
+                puntuacion.punt += (int)configuracionFrutas[configuraciones].gold;
+            }
         }
-    } 
+    }
+    private int GetConfiguracionFruta(GameObject fruta)
+    {
+
+        string tagFruta = fruta.tag;
+
+        int configuraciones = configuracionFrutas.FindIndex(cf => cf.name == tagFruta);
+
+        return configuraciones;
+    }
 }
