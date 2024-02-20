5bdfc3dd91715f08d119e0a2a90e0d03d99c0026
AdriaLama <adria.lama@enti.cat>
2024-02-20 10:27:35 +0100

    Main menu

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -30,4 +31,10 @@ public class GameManager : MonoBehaviour
     {
         
     }
+
+    public void CargarJuego()
+    {
+        SceneManager.LoadScene("Juego");
+    }
+
 }
