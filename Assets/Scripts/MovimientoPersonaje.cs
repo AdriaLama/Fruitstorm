25278ab779ef3097617ea396e7b86044330e358a
AdriaLama <adria.lama@enti.cat>
2024-03-05 20:27:38 +0100

    dsda

--- Assets/Scripts/MovimientoPersonaje.cs
+++ Assets/Scripts/MovimientoPersonaje.cs
@@ -7,7 +7,23 @@ public class MovimientoPersonaje : MonoBehaviour
  
     public float speed;
     private bool invertido = false;
-   
+    public Sprite jodebayayiyas;
+    public Sprite Player;
+    public SpriteRenderer spriteRenderer;
+    public Animator anim;
+
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+       
+    }
+
     void Update()
     {
         float horizontal;
@@ -18,10 +34,29 @@ public class MovimientoPersonaje : MonoBehaviour
         else
         {
             horizontal = Input.GetAxis("Horizontal");
+
+
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                spriteRenderer.sprite = jodebayayiyas;
+                spriteRenderer.transform.localScale = new Vector3(1, 1, 1);
+                anim.SetTrigger("jodiendopachiris");
+
+
+            }
+            else if (Input.GetKey(KeyCode.RightArrow))
+            {
+                spriteRenderer.sprite = jodebayayiyas;
+            }
+            else
+            {
+                spriteRenderer.sprite = Player;
+            }
         }
         transform.position += new Vector3(horizontal, 0, 0) * speed * Time.deltaTime;
 
-     
+
+
     }
 
     public void Invertido()
