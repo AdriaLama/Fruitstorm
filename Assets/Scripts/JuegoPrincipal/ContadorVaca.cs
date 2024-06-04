5d83460f8a9fa1201789c869d4adc65d08ce1ca7
AdriaLama <adria.lama@enti.cat>
2024-06-04 18:34:51 +0200

    j

--- Assets/Scripts/JuegoPrincipal/ContadorVaca.cs
+++ Assets/Scripts/JuegoPrincipal/ContadorVaca.cs
@@ -55,7 +55,7 @@ public class ContadorVaca : MonoBehaviour
 
     public IEnumerator countdownVaca()
     {
-        int countdown = 25;
+        int countdown = 20;
         contVaca.gameObject.SetActive(true);
 
         while (countdown > 0)
